0e64aff510d8fa67ec7d00a6f62429c75174ce51
kakakoi <56661650+kakakoi@users.noreply.github.com>
2021-11-16 14:50:50 +0900

    test pipeline 2.3

--- cdk/lib/pipelines-stack.ts
+++ cdk/lib/pipelines-stack.ts
@@ -1,6 +1,7 @@
 import { EcrStack } from './ecr-stack';
 import { Construct, Stage, Stack, StackProps, StageProps, SecretValue } from '@aws-cdk/core';
 import { CodePipeline, CodePipelineSource, ShellStep } from '@aws-cdk/pipelines';
+import { ContextHelper } from './helper/context-helper';
 
 /**
  * パイプラインを定義するStack
@@ -9,9 +10,13 @@ export class PipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const contextHelper = new ContextHelper(scope) //name helper
+
+    const account = process.env.CDK_DEFAULT_ACCOUNT
+    const region = process.env.CDK_DEFAULT_REGION
 
     const source = CodePipelineSource.connection('kakakoi/fargate-rds-cdk', 'main', {
-      connectionArn: 'arn:aws:codestar-connections:ap-northeast-1:674115544340:connection/a5e589b5-a2fb-4a9b-ba5d-bb945e058040',
+      connectionArn: `arn:aws:codestar-connections:${region}:${account}:connection/a5e589b5-a2fb-4a9b-ba5d-bb945e058040`,
     })
 
     const pipeline = new CodePipeline(this, 'Pipeline', {
@@ -30,16 +35,21 @@ export class PipelineStack extends Stack {
 
     const stage = new PipelinesStage(this, 'PipelinesStage', {
       env: {
-        account: process.env.CDK_DEFAULT_ACCOUNT,
-        region: process.env.CDK_DEFAULT_REGION,
+        account: account,
+        region: region,
       }
     })
+
+    const ecrName = contextHelper.generate("ecr")
 
     pipeline.addStage(stage, {
       post: [new ShellStep('DockerBuild', {
         input: source,
         commands: [
-          'pwd'
+          `aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${account}.dkr.ecr.${region}.amazonaws.com`,
+          `docker build -t ${ecrName} .`,
+          `docker tag ${ecrName}:latest ${account}.dkr.ecr.${region}.amazonaws.com/${ecrName}:latest`,
+          `docker push ${account}.dkr.ecr.${region}.amazonaws.com/${ecrName}:latest`
         ]
       })
       ]
