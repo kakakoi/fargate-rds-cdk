2e5bde92369cb259f230f77b29313162380a86f4
kakakoi <56661650+kakakoi@users.noreply.github.com>
2021-11-16 18:48:07 +0900

    test pipeline add ecs stack

--- cdk/lib/pipelines-stack.ts
+++ cdk/lib/pipelines-stack.ts
@@ -117,6 +117,6 @@ class PipelinesEcrStage extends Stage {
  class PipelinesEcsStage extends Stage {
   constructor(scope: Construct, id: string, props?: StageProps) {
     super(scope, id, props);
-    const ecsStack = new EcsStack(this, 'ecr');
+    const ecsStack = new EcsStack(this, 'ecs');
   }
 }
